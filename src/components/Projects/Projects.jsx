8db685958f536fcc02076429529283b2721e23a9
mkinfrared <mkinfrared@gmail.com>
2018-07-13 10:40:49 -0600

    added group project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -46,212 +46,27 @@ function Projects() {
 						duration={1000}>
 					<div className="project">
 						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
+							<img src="/images/spootify.jpg" alt="spootify"/>
+							<a href="https://www.spootify.org" target="_blank">
 								<div className='description'>
 									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
+										The Idea behind this project was to clone Spotify desktop application. Using
+										Spotify's web API and playback SDK, we we're able to recreate a sleek looking
+										Spotify clone as well as producing music playback. My main responsibilty was to
+										achieve authentication using a users Spotify information through PassportJs,
+										setting up the NodeJs server to connect to th Spotify API, and initializing and
+										connecting the Spotify SDK music player.
 									</p>
 								</div>
 							</a>
 						</div>
 						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
+							<a href="https://www.spootify.org" target="_blank">
+								<h4>Spootify</h4>
+								<p>Music Player App</p>
 							</a>
 							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={500}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={600}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={700}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={800}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={900}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
-									<img src='/icons/Octicons-mark-github.svg' alt=""/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</Bounce>
-				<Bounce left
-						delay={1000}
-						duration={1000}>
-					<div className="project">
-						<div className="snapshot">
-							<img src="/images/majestic.jpg" alt="majestic"/>
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<div className='description'>
-									<p>
-										Movie theater app that helps movie theater administrators to add movies to the
-										database, mark movies they want to show at their theater, create showtimes for
-										each
-										movie, track sold tickets, and for users to choose showtimes and seats and make
-										payments on the website.
-									</p>
-								</div>
-							</a>
-						</div>
-						<div className="brief-desc">
-							<a href="https://majestic.mkorotkov.com" target="_blank">
-								<h4>Majestic</h4>
-								<p>Movie Theater App</p>
-							</a>
-							<div>
-								<a href="https://github.com/mkinfrared/personal-project-i" target='_blank'>
+								<a href="https://github.com/spootify/spootify" target='_blank'>
 									<img src='/icons/Octicons-mark-github.svg' alt=""/>
 								</a>
 							</div>
