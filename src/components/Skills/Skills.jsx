8f1ad653dd27d7583bac66d18218e6115796e27a
mkinfrared <mkinfrared@gmail.com>
2018-08-17 11:59:36 +1000

    added youracclaim badge

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,14 +3,32 @@ import Blocks from '../Blocks/Blocks';
 import {Bounce} from 'react-reveal';
 
 function Skills(props) {
+	function getBadge() {
+		var s   = document.createElement('script');
+		s.type  = 'text/javascript';
+		s.async = true;
+		s.src   = '//cdn.youracclaim.com/assets/utilities/embed.js';
+		var o   = document.getElementsByTagName('script')[0];
+		o.parentNode.insertBefore(s, o);
+	}
+
+	getBadge();
+
 	return (
 		<div id='skills' className='skills'>
 			<Bounce top
-					delay={100}
-					duration={1000}>
+					  delay={100}
+					  duration={1000}>
 				<h2>My skills</h2>
 			</Bounce>
 			<Blocks/>
+			<div className="badges">
+				<div className='badge'
+					  data-iframe-width="150"
+					  data-iframe-height="270"
+					  data-share-badge-id="265a674f-a791-4371-9f39-ac61fbb8cdd6">
+				</div>
+			</div>
 		</div>
 	);
 }
