b31eb8452c772b19046b5b2d87ca3fc551b1f673
mkinfrared <mkinfrared@gmail.com>
2018-12-20 11:03:56 +1000

    added robots.txt

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const express     = require('express'),
-	  path        = require('path'),
-	  compression = require('compression');
+const express = require("express"),
+  path = require("path"),
+  compression = require("compression");
 
 const app = express();
 
@@ -10,8 +10,13 @@ app.use(compression());
 
 app.use(express.static(`${__dirname}/../build`));
 
-app.get('*', (req, res, next) => {
-	res.sendFile(path.join(__dirname, '../build/index.html'));
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain");
+  res.send("User-agent: *");
 });
 
-app.listen(3300, () => console.log('Working on port 3333'));
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+app.listen(3300, () => console.log("Working on port 3333"));
